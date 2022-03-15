8e49357959e8923cbff5c9a23c82a62cdb8cbf0a
JuanVillega <73851081+JuanVillega@users.noreply.github.com>
2022-03-15 16:31:06 -0400

    Primera Actividad, Juan Mendoza

---
+++ Actividad1 Juan Mendoza.py
@@ -0,0 +1,4 @@
+#Primera Actividad Almacenamiento en Variables e impresión
+NombreCompleto = "Juan Manuel Mendoza Colmenarez"
+# llamamos a la función print(), y devolvamos una cadena multiple
+print("Hola Grop1 Soy:" , NombreCompleto)
